4b28d2b3e02c88122c259b670bd8cd1ab47fb098
OskarHD <oskar.h.dinesen@gmail.com>
2023-09-20 22:53:37 +0200

    First iteration of 24bit to 16bit WAV batch converter

---
+++ README.md
@@ -0,0 +1,6 @@
+### Audio bit converter
+
+This is a tool for converting 24bit WAV files to 16bit WAV using the Rust crate Hound.
+
+
+
---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use hound;
+use rand::Rng;
+use std::path::Path;
+
+fn main() {
+
+    let file1 = "ND2sample1.wav";
+
+    convert24to16bit(file1);
+
+}
+
+
+fn convert24to16bit(file: &str) {
+
+    if !Path::new(file).exists() {
+        panic!("Non-existing file: {}", file);
+    }
+    let reader = hound::WavReader::open(file);
+
+    println!("{}", reader.as_ref().unwrap().spec().bits_per_sample);
+    println!("{}", reader.as_ref().unwrap().spec().sample_rate);
+    
+    let mpcspec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 44100,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+
+     
+    match reader {
+        Ok(ref v) => v,
+        Err(e) => {
+            panic!("Error loading file! {}", e);
+            },
+    };
+
+    let mut writer = hound::WavWriter::create("outputtest_08dither_amp01.wav",mpcspec).unwrap();
+
+    let amplitude = 0.99; 
+
+    // initializing stuff for dithering.
+    let mut rng = rand::thread_rng();
+    let scale: f64 = 256.0; // = 2^24 / 2^16. 
+    let scalingfactor: f64 = 0.8;
+    let mut err: f64 = 0.0;
+
+    // dithering and writing samples.
+    for sample in reader.expect("Can't read reader").samples::<i32>() {
+        
+        let dither: f64 = rng.gen_range(-1.0..1.0);
+        let scaled_sample = sample.unwrap() /scale as i32;
+        let scaled_dithered_sample = (scaled_sample as f64 + dither + scalingfactor * err).floor();
+
+        err = (scaled_sample as f64 - scaled_dithered_sample).into();
+        
+        writer.write_sample((scaled_dithered_sample * amplitude) as i16).unwrap();
+    }
+    
+
+    match writer.finalize() {
+        Ok(file) => file,
+        Err(e) => println!("Finalization faild due to: {}", e),
+    } ;
+
+    println!("Finished Processing!");
+ 
+}
