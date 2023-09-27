e0566b1cde04bfc2d184e74f76df922802d7302e
OskarHD <oskar.h.dinesen@gmail.com>
2023-09-27 19:05:42 +0200

    Added the ability to specify destination folder of converted files, as well as testing

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,11 @@ struct Args {
 
     #[arg(short,long, default_value_t = 16)]
     bits: u16,
-    
+
+    #[arg(short,long, default_value_t = String::from(""))]
+    destination: String,
+
+
     #[arg(short,long,default_value_t = true)]
     verbose: bool,
 }
@@ -28,15 +32,19 @@ fn main() {
         sample_format: hound::SampleFormat::Int,
     };
 
-    bitconverter(&file.filename, spec, file.verbose);
+    bitconverter(&file.filename, spec, file.verbose, file.destination);
 }
 
-fn bitconverter(file: &str, spec: hound::WavSpec, verbose: bool) {
+fn bitconverter(file: &str, spec: hound::WavSpec, verbose: bool, destination: String) {
 
     if !Path::new(file).exists() {
         panic!("Non-existing file: {}", file);
     }
 
+    match spec.bits_per_sample {
+        n if (n == 8) | (n == 16) | (n == 24) => n,
+        _ => panic!("Please choose a valid bit-rate!"),
+    };
 
     let reader = hound::WavReader::open(file);
     
@@ -54,9 +62,13 @@ fn bitconverter(file: &str, spec: hound::WavSpec, verbose: bool) {
         _ => "no_change",
     };
 
+    let mut prefix = "".to_string();
+    if destination != "" {
+        prefix = format!("{}{}", destination , "/".to_string());
+    }
 
 
-    let newfilename = format!("{}{}",file.strip_suffix(".wav").expect("File does not have .wav extension"),suffix);
+    let newfilename = format!("{}{}{}",prefix,file.strip_suffix(".wav").expect("File does not have .wav extension"),suffix);
 
     let mut writer = hound::WavWriter::create(newfilename.clone(),spec).unwrap();
 
@@ -64,12 +76,10 @@ fn bitconverter(file: &str, spec: hound::WavSpec, verbose: bool) {
 
     // Calculating bit conversion scale factor:
     let originalbitsize = reader.as_ref().unwrap().spec().bits_per_sample;
-    println!("{}", 2^originalbitsize);
 
     let base: i32 = 2;
     let scale: f64 = (base.pow((originalbitsize - spec.bits_per_sample).into())).into();
     
-    println!("{}", scale);
     // initializing stuff for dithering.
     let mut rng = rand::thread_rng();
     let scalingfactor: f64 = 0.8;
@@ -88,24 +98,117 @@ fn bitconverter(file: &str, spec: hound::WavSpec, verbose: bool) {
             8 => writer.write_sample((scaled_dithered_sample * amplitude) as i8).unwrap(),
             16 =>  writer.write_sample((scaled_dithered_sample * amplitude) as i16).unwrap(),
             24 =>  writer.write_sample((scaled_dithered_sample * amplitude) as i32).unwrap(),
-            _ => writer.write_sample((scaled_dithered_sample * amplitude) as i32).unwrap(),
+            32 => writer.write_sample((scaled_dithered_sample * amplitude) as i32).unwrap(),
+            _ => panic!("Bit-rate is invalid!"),
         }
     }
-    
-
+   
     match writer.finalize() {
         Ok(file) => file,
         Err(e) => println!("Finalization faild due to: {}", e),
-    } ;
+    };
 
 
     if verbose {
-        let readercheck = hound::WavReader::open(newfilename.clone());
-        println!("Info on processed file: ");
-        println!("sample rate: {}", readercheck.as_ref().unwrap().spec().sample_rate);
-        println!("bits: {}", readercheck.as_ref().unwrap().spec().bits_per_sample);
+
+        match spec.bits_per_sample {
+            n if n <= 8 => println!("Bit-rate is {} and is saved as 8-bit", n),
+            n if n <= 16 =>  println!("Bit-rate is {} and is saved as 16-bit", n),
+            n if n <= 24 =>  println!("Bit-rate is {} and is saved as 24-bit", n),
+            n =>  println!("Bit-rate is {} and is saved as 32-bit", n),
+        };
+        //
+        // let readercheck = hound::WavReader::open(newfilename.clone());
+        // println!("Info on processed file: ");
+        // println!("sample rate: {}", readercheck.as_ref().unwrap().spec().sample_rate);
+        // println!("bits: {}", readercheck.as_ref().unwrap().spec().bits_per_sample);
     }
 
  
     println!("Finished Processing!");
+}
+
+
+#[test]
+fn test8bit(){
+    let filename = "testfiles/ND2sample1.wav";
+
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 44100,
+        bits_per_sample: 8,
+        sample_format: hound::SampleFormat::Int,
+    };
+
+    bitconverter(&filename, spec, false , "".to_string());
+    let readercheck = hound::WavReader::open("ND2sample1_8bit.wav");
+        // println!("Info on processed file: ");
+        // println!("sample rate: {}", readercheck.as_ref().unwrap().spec().sample_rate);
+        println!("bits: {}", readercheck.as_ref().unwrap().spec().bits_per_sample);
+
+
+    assert_eq!(spec.bits_per_sample,readercheck.as_ref().unwrap().spec().bits_per_sample);
+
+}
+
+#[test]
+fn test16bit(){
+    let filename = "testfiles/ND2sample1.wav";
+
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 44100,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+
+    bitconverter(&filename, spec, false , "".to_string());
+    let readercheck = hound::WavReader::open("ND2sample1_16bit.wav");
+        // println!("Info on processed file: ");
+        // println!("sample rate: {}", readercheck.as_ref().unwrap().spec().sample_rate);
+        println!("bits: {}", readercheck.as_ref().unwrap().spec().bits_per_sample);
+
+
+    assert_eq!(spec.bits_per_sample,readercheck.as_ref().unwrap().spec().bits_per_sample);
+
+}
+
+
+#[test]
+fn test24bit(){
+    let filename = "testfiles/ND2sample1.wav";
+
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 44100,
+        bits_per_sample: 24,
+        sample_format: hound::SampleFormat::Int,
+    };
+
+    bitconverter(&filename, spec, false, "".to_string() );
+    let readercheck = hound::WavReader::open("ND2sample1_24bit.wav");
+        // println!("Info on processed file: ");
+        // println!("sample rate: {}", readercheck.as_ref().unwrap().spec().sample_rate);
+        println!("bits: {}", readercheck.as_ref().unwrap().spec().bits_per_sample);
+
+
+    assert_eq!(spec.bits_per_sample,readercheck.as_ref().unwrap().spec().bits_per_sample);
+
+}
+
+
+#[test]
+#[should_panic]
+fn testinvalidbit(){
+    let filename = "testfiles/ND2sample1.wav";
+
+    let spec = hound::WavSpec {
+        channels: 1,
+        sample_rate: 44100,
+        bits_per_sample: 12,
+        sample_format: hound::SampleFormat::Int,
+    };
+
+    bitconverter(&filename, spec, false , "".to_string());
+
 }
